f0a9313134f1658877e54bf91bd29802ddb470ad
Divyansh <85442869+divyaannsh@users.noreply.github.com>
2024-09-15 15:24:47 +0530

    Components

---
+++ src/components/list/chatlist/Chatlist.jsx
@@ -0,0 +1,42 @@
+
+import { useState } from 'react'
+import "./chatlist.css"
+const Chatlist = () => {
+
+const [addMode,setAddMode]= useState(false)
+  return (
+    <div className='chatlist'>
+      <div className="search">
+        <div className="searchbar">
+            <img src="/search.png" alt="" />
+            <input type="text"  placeholder='search'/>
+       </div>
+       <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className='add' onClick={() => setAddMode((prev) => !prev)}/>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hello</p>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Chatlist
